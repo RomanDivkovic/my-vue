7eb58b95e9b3b72e2e9118d4b5d14d873b5c4de1
RomanDivkovic <divkovicroman@gmail.com>
2021-05-12 16:19:15 +0200

    Added vue.config.js and just changed my readme

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  pwa: {
+    manifestOptions: {
+      background_color: 'hotpink'
+    },
+    name: 'My labb project',
+    themeColor: 'steelblue',
+    workboxOptions: {
+      runtimeCaching: [
+        {
+          handler: 'NetworkFirst',
+          options: {
+            networkTimeoutSeconds: 5
+          },
+          urlPattern:
+            'https://official-joke-api.appspot.com/jokes/programming/ten'
+        },
+        {
+          handler: 'NetworkFirst',
+          options: {
+            networkTimeoutSeconds: 5
+          },
+          urlPattern:
+            'https://api.openweathermap.org/data/2.5/weather?q=partille&units=metric&APPID=6e14ee8cdb51cf3777c97628f3ae1998'
+        }
+      ]
+    }
+  }
+}
